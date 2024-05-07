780a50bac6fede175ee797f664bd31ab760e42c1
kallegre <admin@newvm.myguest.virtualbox.org>
2024-05-07 23:35:32 +0200

    front update

--- new/static/js/script.js
+++ new/static/js/script.js
@@ -3,7 +3,30 @@ const extanded = document.querySelector('.extanded-menu');
 const phone = document.querySelector('.phone-menu');
 const blured = document.querySelector('.blured');
 
-extanded.style.display = 'none';
+const MenuPref = () => {
+  const menuState = localStorage.getItem('menu');
+  if (window.innerWidth > 1224) {
+    if (menuState === 'short') {
+      short.style.display = 'flex';
+      extanded.style.display = 'none';
+    }
+    else if (menuState === 'extanded') {
+      short.style.display = 'none';
+      extanded.style.display = 'flex';
+    }
+  }
+  else if (window.innerWidth < 768){
+    short.style.display = 'none';
+    extanded.style.display = 'none';
+  }
+  else {
+    short.style.display = 'flex';
+    extanded.style.display = 'none';
+  }
+}
+
+MenuPref();
+window.onresize = MenuPref;
 
 const showMenu = () => {
   if (window.innerWidth < 1224) {
@@ -21,10 +44,12 @@ const showMenu = () => {
     if (short.style.display === 'none') {
       short.style.display = 'flex';
       extanded.style.display = 'none';
+      localStorage.setItem('menu', 'short');
     }
     else {
       short.style.display = 'none';
       extanded.style.display = 'flex';
+      localStorage.setItem('menu', 'extanded');
     }
   }
 }
