6f54657f803f7c406722816dfba81f2c675bde21
root <root@kigo.myguest.virtualbox.org>
2024-08-12 15:14:53 +0200

    test

---
+++ new/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Wait for PostgreSQL to be available
+echo "Waiting for PostgreSQL to be available..."
+while ! pg_isready -h "db" -p 5432 -U "postgres"; do
+  sleep 1
+done
+echo "PostgreSQL is up!"
+
+# Run Django migrations
+echo "Applying database migrations..."
+python manage.py makemigrations
+python manage.py migrate
+python manage.py update_status
+python manage.py shell -c "from myaccount import __init__"
+
+
+# Start the Django application
+exec "$@"
